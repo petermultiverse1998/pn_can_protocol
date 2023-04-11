3a92d2730193cc55c7df78db68f6015f48c26393
niruja-gh <nirujaghimire@gmail.com>
2023-04-11 10:16:54 +0545

    update

--- pn_can_protocol/Debug/can_com/base_layer_can.d
+++
@@ -1,4 +0,0 @@
-can_com/base_layer_can.o: ../can_com/base_layer_can.c \
- ../can_com/base_layer_can.h ../can_com/que.h
-../can_com/base_layer_can.h:
-../can_com/que.h:
--- pn_can_protocol/Debug/can_com/que.d
+++
@@ -1,2 +0,0 @@
-can_com/que.o: ../can_com/que.c ../can_com/que.h
-../can_com/que.h:
