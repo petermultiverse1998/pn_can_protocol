4d1d97fe368e0b27204dfdbbb096b8540d1619b7
niruja-gh <nirujaghimire@gmail.com>
2023-07-07 16:12:03 +0545

    update

--- pn_can_protocol/pn_protocol/pn_can_sync_layer_test.c
+++ pn_can_protocol/pn_protocol/pn_can_sync_layer_test.c
@@ -11,12 +11,16 @@
 extern CRC_HandleTypeDef hcrc;
 
 static SyncLayerCanLink link1 = { 0x1, 0x2, 0x3, 0x4, 0x5, 0x6, 0x7 };
+static SyncLayerCanLink link2 = { 0x11, 0x12, 0x13, 0x14, 0x15, 0x16, 0x17 };
 
 SyncLayerCanData data1;
 SyncLayerCanData data2;
 SyncLayerCanData data3;
 
-uint8_t tx_bytes[] = { 1, 2, 3, 4, [9]=10 };
+uint8_t tx_bytes1[16] = { 1, 2, 3, 4, [9]=10 };
+uint8_t tx_bytes2[16] = { 1, 5, 3, 7 };
+uint8_t tx_bytes3[16] = { 2, 4, 6, 8 };
+
 uint8_t rx_bytes1[16];
 uint8_t rx_bytes2[16];
 uint8_t rx_bytes3[16];
@@ -63,8 +67,14 @@ static void canRxInterruptRx() {
 
 	uint32_t id = *(uint32_t*) bytes;
 	if (id == data1.id || data1.id == rx_header.ExtId) {
-		StaticSyncLayerCan.rxReceiveThread(&link1, &data1, rx_header.ExtId, bytes,
-				rx_header.DLC);
+		StaticSyncLayerCan.rxReceiveThread(&link1, &data1, rx_header.ExtId,
+				bytes, rx_header.DLC);
+	} else if (id == data2.id || data2.id == rx_header.ExtId) {
+		StaticSyncLayerCan.rxReceiveThread(&link1, &data2, rx_header.ExtId,
+				bytes, rx_header.DLC);
+	} else if (id == data3.id || data3.id == rx_header.ExtId) {
+		StaticSyncLayerCan.rxReceiveThread(&link2, &data3, rx_header.ExtId,
+				bytes, rx_header.DLC);
 	}
 
 }
@@ -78,8 +88,8 @@ static void canRxInterruptTx() {
 
 	uint32_t id = *(uint32_t*) bytes;
 	if (id == data1.id || data1.id == rx_header.ExtId) {
-		StaticSyncLayerCan.txReceiveThread(&link1, &data1, rx_header.ExtId, bytes,
-				rx_header.DLC);
+		StaticSyncLayerCan.txReceiveThread(&link1, &data1, rx_header.ExtId,
+				bytes, rx_header.DLC);
 	}
 
 }
@@ -103,18 +113,27 @@ static uint8_t canSend(uint32_t id, uint8_t *bytes, uint8_t len) {
 }
 
 void txCallback(SyncLayerCanLink *link, SyncLayerCanData *data, uint8_t status) {
-
+	if (!status) {
+		printf("Data failed\n");
+		data->track = SYNC_LAYER_CAN_START_REQUEST;
+		return;
+	}
+	printf("ID : 0x%0x", (int) data->id);
+	for (int i = 0; i < data->size; i++)
+		printf(" %d ", data->bytes[i]);
+	printf("status : %d\n", status);
+	data->track = SYNC_LAYER_CAN_START_REQUEST;
 }
 
 void rxCallback(SyncLayerCanLink *link, SyncLayerCanData *data, uint8_t status) {
 	if (!status) {
-//		printf("Data failed\n");
+		printf("Data failed\n");
 		data->track = SYNC_LAYER_CAN_START_REQUEST;
 		return;
 	}
 
-	printf("ID : 0x%0x",(int) data->id);
-	for (int i = 0; i < 8; i++)
+	printf("ID : 0x%0x", (int) data->id);
+	for (int i = 0; i < data->size; i++)
 		printf(" %d ", data->bytes[i]);
 	printf("status : %d\n", status);
 	data->track = SYNC_LAYER_CAN_START_REQUEST;
@@ -148,6 +167,8 @@ static void runRx() {
 
 	while (1) {
 		StaticSyncLayerCan.rxSendThread(&link1, &data1, canSend, rxCallback);
+		StaticSyncLayerCan.rxSendThread(&link1, &data2, canSend, rxCallback);
+		StaticSyncLayerCan.rxSendThread(&link2, &data3, canSend, rxCallback);
 
 	}
 }
@@ -156,22 +177,22 @@ static void runTx() {
 	canInit();
 
 	data1.id = 0xA;
-	data1.bytes = rx_bytes1;
-	data1.size = sizeof(rx_bytes1);
+	data1.bytes = tx_bytes1;
+	data1.size = sizeof(tx_bytes1);
 	data1.track = SYNC_LAYER_CAN_START_REQUEST;
 	data1.data_retry = 0;
 	data1.dynamically_alocated = 0;
 
 	data2.id = 0xB;
-	data2.bytes = rx_bytes2;
-	data2.size = sizeof(rx_bytes2);
+	data2.bytes = tx_bytes2;
+	data2.size = sizeof(tx_bytes2);
 	data2.track = SYNC_LAYER_CAN_START_REQUEST;
 	data2.data_retry = 0;
 	data2.dynamically_alocated = 0;
 
 	data3.id = 0xC;
-	data3.bytes = rx_bytes3;
-	data3.size = sizeof(rx_bytes3);
+	data3.bytes = tx_bytes3;
+	data3.size = sizeof(tx_bytes3);
 	data3.track = SYNC_LAYER_CAN_START_REQUEST;
 	data3.data_retry = 0;
 	data3.dynamically_alocated = 0;
@@ -183,5 +204,6 @@ static void runTx() {
 	}
 }
 
-struct SyncLayerCanTest StaticSyncLayerCanTest = {
-		.canRxInterruptRx = canRxInterruptRx,.canRxInterruptTx = canRxInterruptTx, .runRx = runRx, .runTx = runTx};
+struct SyncLayerCanTest StaticSyncLayerCanTest = { .canRxInterruptRx =
+		canRxInterruptRx, .canRxInterruptTx = canRxInterruptTx, .runRx = runRx,
+		.runTx = runTx };
